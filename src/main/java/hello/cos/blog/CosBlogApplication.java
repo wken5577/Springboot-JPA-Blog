507b9072bcd3be3e605db8f16ab424494c4931a2
wken5577 <wken5577@hanmail.net>
2022-04-04 15:53:56 +0900

    환경세팅 완료

---
+++ src/main/java/hello/cos/blog/CosBlogApplication.java
@@ -0,0 +1,13 @@
+package hello.cos.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/cos/blog/CosBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.cos.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
